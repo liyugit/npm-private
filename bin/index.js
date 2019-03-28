592975a53a4104c33049cc5355f74667f2c730f2
liyu3 <liyu3@guazi.com>
2019-03-29 00:17:00 +0800

    done

---
+++ bin/index.js
@@ -0,0 +1,15 @@
+const path = require('path')
+const fs = require('fs')
+
+let npmPrivate = require('../lib')
+let clientRoot =  path.resolve(__dirname) 
+let packageJsonPath = path.join(clientRoot,'../client/package.json')
+debugger
+try{
+    let content = fs.readFileSync(packageJsonPath,'utf8')
+    content = JSON.parse(content)
+    npmPrivate.install(content.dependencies)
+}
+catch(e){
+    console.log(e)
+}
